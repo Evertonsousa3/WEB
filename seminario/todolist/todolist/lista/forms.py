18be3c4cb2491ab4af2553b5f915966cdf9929c3
Everton Sousa Oliveira <105239083+Evertonsousa3@users.noreply.github.com>
2024-01-22 19:45:17 -0300

    todolist

--- seminario/todolist/todolist/lista/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-from .models import Tarefa
-
-class TarefaForm(forms.ModelForm):
-    class Meta:
-        model = Tarefa
-        fields = ['descricao', 'concluida']
