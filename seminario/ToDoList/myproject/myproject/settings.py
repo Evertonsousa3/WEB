c6b2ed3ff12b691528e4a93deaccb422db3faf96
Stephen Barreto <10745123+stephenbarreto@users.noreply.github.com>
2024-01-23 14:03:35 -0300

    Update settings.py

--- seminario/ToDoList/myproject/myproject/settings.py
+++ seminario/ToDoList/myproject/myproject/settings.py
@@ -143,7 +143,7 @@ STATICFILES_FINDERS = (
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
